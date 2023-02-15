dc65a561e2e96f3678630dcdc42284df6bcf1a6a
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-02-15 11:38:33 +0100

    Installed Firebase

---
+++ src/Backend/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, GoogleAuthProvider } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXEmJl5XNkq2COBrA8YVFZEV0JbbZU2g4",
+  authDomain: "academia-voting-app.firebaseapp.com",
+  projectId: "academia-voting-app",
+  storageBucket: "academia-voting-app.appspot.com",
+  messagingSenderId: "417346351649",
+  appId: "1:417346351649:web:3abaedc2f2596cee096960"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(app);
